2aa6fc5b37b950da7ec8f36b7968dd2d3892de59
Zbiku <Zbiku@MSI>
2023-10-26 22:44:01 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿class Zad
+{
+    static void Main(string[] args)
+    {
+        double a, b, c;
+        Console.WriteLine("Podaj pierwszą liczbę: ");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj drugą liczbę: ");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj trzecią liczbę: ");
+        c = double.Parse(Console.ReadLine());
+        if (a < b && a < c)
+        {
+            Console.WriteLine("Najmniejsza liczba wynosi " + a);
+        }
+        else if (b < c && b < c)
+        {
+            Console.WriteLine("Najmniejsza liczba wynosi " + b);
+        }
+        else
+        {
+            Console.WriteLine("Najmniejsza liczba wynosi " + c);
+        }
+        Console.ReadKey();
+    }
+}
